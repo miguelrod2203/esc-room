ef8052486400b239c6b86717ebe4b628e846843e
Miguel Rodriguez <miguelrod2203@gmail.com>
2023-07-03 20:59:07 -0400

    navbar inicial listo

---
+++ src/componentes/NavBar/NavBar.js
@@ -0,0 +1,47 @@
+import { useState } from "react"
+import { AiOutlineMenu } from "react-icons/ai";
+
+
+
+export const Navbar = () => {
+    const [mostrarMenu, setMostrarMenu] = useState(false);
+
+    const toggleNavar = () => {
+        setMostrarMenu(!mostrarMenu)
+        console.log("click")
+    }
+
+
+
+    return (
+        <header>
+
+            <nav className="bg-gray-700 border-b-2 border-yellow-300">
+                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <div className="flex items-center">
+                        <a href="/">
+                            <img className="h-10 w-auto mr-1 rounded-xl" src="../../imagenes/logo5.jpeg" alt="Logo"/>
+                        </a>            
+                    </div>
+                    <button onClick={toggleNavar} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-lg text-yellow-300 rounded-lg md:hidden hover:text-white focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
+                        <span className="sr-only">Open main menu</span>
+                        <AiOutlineMenu/>
+                    </button>
+                    <div className={`${ mostrarMenu ? "flex" : "hidden"}  w-full md:block md:w-auto justify-center`}>
+                        <div className="font-medium flex flex-col p-1 md:p-0 mt-4 md:flex-row md:mt-0 md:border-0">
+                            <a href="/" className="text-yellow-300 hover:text-white px-3 py-1 text-sm font-medium border-b-2 border-transparent hover:border-b-2 hover:border-yellow-300 duration-500">¿Que es?</a>
+                            <a href="/" className="text-yellow-300 hover:text-white px-3 py-1 text-sm font-medium border-b-2 border-transparent hover:border-b-2 hover:border-yellow-300 duration-500">Habitaciones</a>
+                            <a href="/" className="text-yellow-300 hover:text-white px-3 py-1 text-sm font-medium border-b-2 border-transparent hover:border-b-2 hover:border-yellow-300 duration-500">Precios</a>
+                            <a href="/" className="text-yellow-300 hover:text-white px-3 py-1 text-sm font-medium border-b-2 border-transparent hover:border-b-2 hover:border-yellow-300 duration-500">Preguntas Frecuentes</a>
+                            <a href="/" className="text-yellow-300 hover:text-white px-3 py-1 text-sm font-medium border-b-2 border-transparent hover:border-b-2 hover:border-yellow-300 duration-500">Contactos</a>
+                            <a href="/" className="text-yellow-300 hover:text-white px-3 py-1 text-sm font-medium border-b-2 border-transparent hover:border-b-2 hover:border-yellow-300 duration-500">Reserva Ya!</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+
+
+        </header>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'regal-blue': '#EC640B',
+      },
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(255, 255, 255, 0.25)',
+        '4xl': [
+            '0 35px 35px rgba(255, 255, 255, 0.25)',
+            '0 45px 65px rgba(255, 255, 255, 0.15)'
+        ]
+      }
+    },
   },
   plugins: [],
 }
